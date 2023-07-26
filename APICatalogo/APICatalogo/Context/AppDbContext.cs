22ecf08e9e10d6c57f6b419e890f8ef4b5b90b04
Lenilson Soares <lenilsonp40@gmail.com>
2023-07-26 17:22:05 -0300

    first commit

---
+++ APICatalogo/APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextoptions<AppDbContext> options) : base(options) 
+        {  }
+
+        public Dbset<Categoria> Categorias { get; set; }
+        public Dbset<Produto> Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/APICatalogo/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string sqlServerConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseSqlServer(SqlServerConnection,
+    ServerVersion.AutoDetect(SqlServerConnection)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
