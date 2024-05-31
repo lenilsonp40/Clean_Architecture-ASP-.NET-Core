56d986a21cfaf6a3df3f15797d690bc59cb10234
Lenilson Soares <lenilsonp40@gmail.com>
2024-05-30 21:39:36 -0300

    ControllerProduto

---
+++ ProjetoCatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
