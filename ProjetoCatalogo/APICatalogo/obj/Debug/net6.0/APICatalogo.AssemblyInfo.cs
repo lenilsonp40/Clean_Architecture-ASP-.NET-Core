30ffc1480152ef1d8ad95b1bc17e38ff234f5b1b
Lenilson Soares <lenilsonp40@gmail.com>
2024-07-22 10:22:25 -0300

    up

--- ProjetoCatalogo/APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
+++ ProjetoCatalogo/APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66979508882fb2d04d59c8d657504a3096ebb597")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
